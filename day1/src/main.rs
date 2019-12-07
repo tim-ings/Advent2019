3779e29de2dfc05b0cf2eaef7770150a298a5718
Tim <tim@tim-ings.com>
2019-12-07 22:27:08 +0800

    Implements day 1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    // open our input file
+    let file = File::open("input.txt").unwrap();
+    // create a buf reader to read it
+    let reader = BufReader::new(file);
+
+    let mut total_fuel:u32 = 0;
+
+    for line in reader.lines() {
+        let line:String = line.unwrap(); // we should never fail to read a line, or our input is bad
+        let mass:u32 = line.parse::<u32>().unwrap(); // every line should be an int, or our input is bad
+        let fuel_req:u32 = (mass / 3) - 2;
+        total_fuel += fuel_req;
+    }
+    println!("Total Fuel Req = {}", total_fuel);
 }
